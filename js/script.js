67ca306c834ac8eaff9752e68499e26482f0b33e
Jessica Hernandez Botero <jessicahernandez@JessicasMacBook.ht.home>
2022-09-11 12:41:27 -0400

    Began team page onmousehover events, still need to add 2 bios.

--- js/script.js
+++ js/script.js
@@ -6,16 +6,29 @@
             theVideo = document.querySelectorAll('video');
             theVideo.forEach(video => video.play());
       }
-  
-  
+
+
       function pauseVideo(){
-       
+
         theVideo = document.querySelectorAll('video');
-        
+
         theVideo.forEach(video => video.pause());
       }
 
     playButton.addEventListener('click', playVideo);
-    pauseButton.addEventListener('click', pauseVideo);  
+    pauseButton.addEventListener('click', pauseVideo);
+
+    let img = document.querySelector('img');
+      let start = img.src;
+      let hover = img.getAttribute('data-hover');
+
+      setTimeout(() => {
+        transition_el.classList.remove('activeTran');
+      }, 250);
+    }
+
+      img.onmouseover = () => { img.src = hover; }
+
+      img.onmouseout = () => { img.src = start; }
 
 })();
